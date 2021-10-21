51d41bcee8bd93c1214a9b257ec189633d0dfa2f
syamsulamin <umaminzamin@gmail.com>
2021-10-21 11:53:49 +0700

    add icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,58 @@
 <template>
   <div class="flex justify-content-start">
     <TheSidebar />
-    <nav class="header bg-dark">
-      header
+    <div class="header bg-dark">
+      <div class="flex justify-content-between">
+        <div class="pl-lg pt-sm">
+          <img class="pr-sm" v-for="(item, index) in iconsHeader" :key="index" :src="item['img']" />
+        </div>
+        <div>
+          ok
+        </div>
+      </div>
       <slot></slot>
-    </nav>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 // @ts-ignore
 import TheSidebar from "~/components/TheSidebar.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log(iconsHeader);
+});
+
+const iconsHeader = [
+  {
+    img: "../assets/images/icons/Icon_Arkadia1.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_suara.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_bolatimes.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_hitekno.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_dewiku.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_mobimoto.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_guideku.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_Himedik.png",
+  },
+  {
+    img: "../assets/images/icons/Icon_serbada.png",
+  },
+];
 </script>
 
 <style lang="scss">
